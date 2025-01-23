e2d606db2f2bc521752be8693b4417abe41371fe
Nicholas Nesmith <nnesmith1124@gmail.com>
2025-01-22 20:01:10 -0500

    Intial Commit!

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
