b3676f1c117f0e1eae82653c7783858f550d783f
Hala-Sobeih-hub <halasobeih@gmail.com>
2025-01-27 19:50:36 -0500

    connect to DB

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express()
+const port = process.env.PORT || 8080
+
+//connect to DB
+const connectDB = require('./config/database.js')
+connectDB()
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+  res.send('Hello World')
+})
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-  
+  console.log(`Example app listening on port ${port}`)
+})
