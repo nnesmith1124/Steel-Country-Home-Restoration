aded6c1ff96d01140aa23d757dcd33767f63fdaf
Hala-Sobeih-hub <halasobeih@gmail.com>
2025-01-29 18:49:43 -0500

    service schema created

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
+app.use(cors())
 
 //connect to DB
 const connectDB = require('./config/database.js')
 connectDB()
 
+//* Imports for controllers
+const serviceController = require('./controllers/service-routes.js')
+
+//* Routes
+app.use('/services', serviceController)
+
 //routes
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
