5f273c47d1ce76578b87b37f74b5dbaab36b40a1
Hala-Sobeih-hub <halasobeih@gmail.com>
2025-01-29 19:42:24 -0500

    services CRUD API and front end completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Services from "../pages/Services";
 
 export default function App() {
   return (
     <div>
-      <H1>Steel Country Home Restoration</H1>
+      <h1>Steel Country Home Restoration</h1>
+      <Services />
     </div>
   );
 }
-App();
