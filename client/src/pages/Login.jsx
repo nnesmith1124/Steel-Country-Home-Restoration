6eebb9aeee991291cb52af27e452b0df0451b3ea
Clayb14 <claybesaw9@gmail.com>
2025-02-17 23:14:24 -0500

    front end login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+import "./Login.css";
 
 export default function Login() {
-  return <div>login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3000/api/login", {
+        username,
+        password,
+      });
+
+      if (response.data) {
+        console.log("Login successful, token received:", response.data);
+
+        localStorage.setItem("authToken", response.data);
+      } else {
+        setErrorMessage("Login failed: No token received.");
+      }
+    } catch (error) {
+      setErrorMessage("Could not log in successfully. Please try again.");
+    }
+  };
+
+  return (
+    <div className="login-container">
+     
+      <form onSubmit={handleSubmit}>
+        <h1 className="header">Login</h1>
+        <div className="username">
+          <label>Username</label>
+          <input
+            className="text-input"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="password">
+          <label>Password </label>
+          <input
+            className="text-input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <button className="submit" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
